9b06f4c14b15e7297527eef33a97da1a540eeed2
Jacob Sides <jsides@pyxl.com>
2021-02-12 13:07:36 -0500

    ThreeJS editor camera example running with site as UI overlay

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('Hello from main.js');
+/* eslint-disable no-undef */
+import * as THREE from './threejs/three.module.js';
+import { APP } from './threejs/app.js';
+import { VRButton } from './threejs/VRButton.js';
+
+window.THREE = THREE; // Used by APP Scripts.
+window.VRButton = VRButton; // Used by APP Scripts.
+
+const loader = new THREE.FileLoader();
+loader.load('app.json', (text) => {
+  const player = new APP.Player();
+  player.load(JSON.parse(text));
+  player.setSize(window.innerWidth, window.innerHeight);
+  player.play();
+
+  document.body.appendChild(player.dom);
+
+  window.addEventListener('resize', () => {
+    player.setSize(window.innerWidth, window.innerHeight);
+  });
+});
